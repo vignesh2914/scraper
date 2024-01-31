c90922a465e3f355d418cfbc878ff090e1d106f2
RENGA RAJAN K <maharengarajan46@gmail.com>
2024-01-31 15:40:16 +0530

    latest update

--- updated.py
+++ updated.py
@@ -12,8 +12,7 @@ def create_file_path(user_keywords):
 
 def linkedin_scraper(webpage, keywords, file_path, page_number=0):
     while True:
-        # no of pages  to scrape from the webpage
-        next_page = f"{webpage}&keywords={keywords}&start={page_number * 50}"
+        next_page = f"{webpage}&keywords={keywords}&start={page_number * 25}"
         print(next_page)
 
         try:
@@ -34,7 +33,6 @@ def linkedin_scraper(webpage, keywords, file_path, page_number=0):
         if not jobs:
             break  
 
-        # fetch job details
         for job in jobs:
             job_title = job.find('h3', class_='base-search-card__title').text.strip()
 
@@ -52,7 +50,7 @@ def linkedin_scraper(webpage, keywords, file_path, page_number=0):
 
         print('Data updated')
 
-        page_number = page_number + 1
+        page_number += 1
 
 user_keywords = input("Enter the job keywords: ")
 
